723ad1896f576618bbd09c227f99a9cd43507bc2
patabu <52673651+patabu@users.noreply.github.com>
2023-01-11 02:14:36 +0100

    Sistema struttura delle cartelle

---
+++ Assets/Scripts/Gameplay/Movements/CarMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public Transform transform;
+    public float speedSx = 5f;
+    public float speedDx = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       transform.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < 0)
+        {
+            //transform.eulerAngles = new Vector3(0, 0, -90); 
+            transform.position -= new Vector3(0, speedSx * Time.deltaTime, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(0, speedDx * Time.deltaTime, 0);
+        }
+        if(transform.position.y <= -11)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
